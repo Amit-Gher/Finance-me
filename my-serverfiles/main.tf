b32a8f32a35151e0a9e9ff99cb51f5ae131d6024
Amit-Gher <137607914+Amit-Gher@users.noreply.github.com>
2023-09-28 23:41:22 +0530

    Updated main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region ="ap-south-1"
+  region ="us-east-1"
 }  
 resource "aws_instance" "Node_Server" {
-    ami = "ami-0f5ee92e2d63afc18"
+    ami = "ami-053b0d53c279acc90"
     instance_type = "t2.micro"
     key_name = "Insure-me"
-    vpc_security_group_ids =["sg-07731d49e8595b55f"]
+  //  vpc_security_group_ids =["sg-07731d49e8595b55f"]
 connection {
     type        = "ssh"
     user        = "ec2_user"
