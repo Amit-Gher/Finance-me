9c0173f7b5ea70852fcd23f986d618ff4543b651
Amit-Gher <137607914+Amit-Gher@users.noreply.github.com>
2023-09-29 14:09:35 +0530

    Created provider.tf

---
+++ my-serverfiles/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+required_providers {
+  aws = {
+  source = "hashicorp/aws"
+  }
+ }
+}
+  provider "aws" {
+  region ="ap-south-1"
+  access_key = "AKIAZGROM53IVZ5G6YX5"
+  secret_key = "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"
+}  
