3dcd10fcb06b0191d7938c64b6404c36ac0c2766
Amit-Gher <137607914+Amit-Gher@users.noreply.github.com>
2023-09-29 15:43:49 +0530

    Update provider.tf

--- my-serverfiles/provider.tf
+++ my-serverfiles/provider.tf
@@ -6,7 +6,7 @@ required_providers {
  }
 }
   provider "aws" {
-  region ="ap-south-1"
+  region ="ap-south-1a"
   access_key = "AKIAZGROM53IS6256G4J"
   secret_key = "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"
 }  
